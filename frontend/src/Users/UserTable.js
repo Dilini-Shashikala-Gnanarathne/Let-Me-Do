d0685c784a25557d9c5ad4d6edcb0dfbcc7ab94d
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-06-03 14:50:20 +0530

    User Table Creation and Update Users Page

---
+++ frontend/src/Users/UserTable.js
@@ -0,0 +1,62 @@
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import Grid from '@mui/material/Grid'
+
+function UserTable(props) {
+  return (
+    <Grid >
+      <TableContainer component={Paper}>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Id</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Email</TableCell>
+                    <TableCell>Password</TableCell>
+                    <TableCell>Gender</TableCell>
+                    <TableCell>Action</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {
+                    props.rows.length>0 ? props.rows.map
+                        (row=>(
+                        <TableRow key={row.id} sx={{'&:last-child td, &:last-child th':{border:0}}}>
+                            <TableCell component='th' scope='row'>{row.id}</TableCell>
+                            <TableCell component='th' scope='row'>{row.name}</TableCell>
+                            <TableCell component='th' scope='row'>{row.email}</TableCell>
+                            <TableCell component='th' scope='row'>{row.password}</TableCell>
+                            <TableCell component='th' scope='row'>{row.gender}</TableCell>
+                            <TableCell >
+                                <Button
+                                sx={{margin :'0px 10px'}}
+                                onClick={()=>{}}
+                                >
+                                    Update
+                                </Button>
+                                <Button
+                                sx={{margin :'0px 10px'}}
+                                onClick={()=>{}}
+                                >
+                                    Delete
+                                </Button>
+                            </TableCell>
+                        </TableRow>
+                        ))
+                        : 
+                       (
+                        <TableRow  sx={{'&:last-child td, &:last-child th':{border:0}}}>
+                        <TableCell component='th' scope='row'>No Data</TableCell>
+                        </TableRow>
+                       )
+                    
+                }
+            </TableBody>
+        </Table>
+      </TableContainer>
+    </Grid>
+  )
+}
+
+
+export default UserTable
+
