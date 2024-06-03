6198c7995a13083a1130b2cebf7930b32238a4ed
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-06-03 18:48:47 +0530

    Rest api for get all users

--- backend/App.js
+++ backend/App.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
+const controller = require('./controller');
 app.use(cors());
 app.use(express.urlencoded());
 app.use(express.json());
+app.get('/users', (req,res) => {
+    controller.getAllUsers(users=>{
+        res.send(users);
 
+    
+    
+})
+});
 module.exports =app;
