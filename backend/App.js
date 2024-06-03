f84e6e7a679764f37711921b4ea3c1b0f968472f
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-06-03 18:55:14 +0530

    rest api for get user

--- backend/App.js
+++ backend/App.js
@@ -7,10 +7,12 @@ app.use(express.urlencoded());
 app.use(express.json());
 app.get('/users', (req,res) => {
     controller.getAllUsers(users=>{
-        res.send(users);
-
-    
-    
+        res.send(users);  
+})});
+app.get('/user', (req,res) => {
+    const id=req.query.id;
+    controller.getUser(id,user=>{
+        res.send(user);  
 })
 });
 module.exports =app;
