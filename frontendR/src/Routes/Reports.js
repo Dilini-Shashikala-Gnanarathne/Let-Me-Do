e85392d3f2a6289830ab5e2dcf0647525f78931c
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-06-11 00:12:08 +0530

    Error Fixed

--- frontendR/src/Routes/Reports.js
+++ frontendR/src/Routes/Reports.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './UserTable.css'; // Import your CSS file
-import '../App.css'
-const Reports = () => {
+
+const Reports = ({ rows, selectedUser, deleteUser }) => {
     return (
         <div className="container">
             <table className="user-table">
@@ -9,11 +9,29 @@ const Reports = () => {
                     <tr>
                         <th>Subject Code</th>
                         <th>Grade</th>
-                        <th>Action</th>
+                        <th>Update</th>
+                        <th>Delete</th>
                     </tr>
                 </thead>
                 <tbody>
-                   
+                    {rows.length > 0 ? (
+                        rows.map(row => (
+                            <tr key={row.id}>
+                                <td>{row.id}</td>
+                                <td>{row.name}</td>
+                                <td>
+                                    <button className="action-button" onClick={() => selectedUser({ id: row.id, name: row.name })}>Update</button>
+                                </td>
+                                <td>
+                                <button className="action-button" onClick={() => deleteUser({ id: row.id })}>Delete</button>
+                                </td>
+                            </tr>
+                        ))
+                    ) : (
+                        <tr>
+                            <td colSpan={3} className="no-data">No Data</td>
+                        </tr>
+                    )}
                 </tbody>
             </table>
         </div>
