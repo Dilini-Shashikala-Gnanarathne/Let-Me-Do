e5278755085f54e8959047bea9ac72258c0208fe
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-06-10 22:37:44 +0530

    Created Dashbord

--- frontendR/src/Routes/Dashboard.js
+++ frontendR/src/Routes/Dashboard.js
@@ -1,11 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import '../App.css';
 
-const Dashboard = () => {
+const Dashbord = ({ addUser, submited, data, isEddit, updateUser }) => {
+  const [id, setId] = useState(0);
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    if (!submited) {
+      setId(0);
+      setName('');
+    }
+  }, [submited]);
+
+  useEffect(() => {
+    if (data?.id && data.id !== 0) {
+      setId(data.id);
+      setName(data.name);
+    }
+  }, [data]);
+
   return (
-    <div className="Dashboard">
-      <h1>Dashboard</h1>
+    <div className="container">
+      <h3 className="title">
+        Create an <span className="highlight">account</span>
+      </h3>
+      <form>
+        <div className="form-group">
+          <label htmlFor="id">Enter Course Code</label>
+          <input
+            type="text"
+            id="id"
+            name="id"
+            value={id}
+            onChange={e => setId(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="name">Enter Subject Grade</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <button
+            type="submit"
+            onClick={() => (isEddit ? updateUser({ id, name }) : addUser({ id, name }))}
+          >
+            {isEddit ? 'Update' : 'Add'}
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
 
-export default Dashboard;
+export default Dashbord;
