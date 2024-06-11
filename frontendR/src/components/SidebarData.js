43a2d153561c629d71eee1c19c95b28d9239c4ac
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-06-11 11:58:03 +0530

    Final changes

--- frontendR/src/components/SidebarData.js
+++ frontendR/src/components/SidebarData.js
@@ -10,13 +10,6 @@ export const SidebarData = [
     icon: <img src={DashboardIcon} alt="Dashboard" className="dashboard-icon"/>,
     cName: "nav-text dashboard",
   },
- 
-  {
-    title: "Reports",
-    path: "/reports",
-    icon: <img src={ReportsIcon} alt="Reports" className="reports-icon"/>,
-    cName: "nav-text reports",
-  },
   {
     title: "Result",
     path: "/Result",
