f9ed0f4030c38117ba7607c1e4fbabbd2366c5ce
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-06-20 12:52:09 +0530

    build error fixed

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,29 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port =3001;
-const host= '127.0.0.1';
+const port = 3001;
 const mongoose = require('mongoose');
 const router = require('./router');
+
 app.use(cors());
 app.use(express.json());
 
-const uri= 'mongodb+srv://shashikaladilini11:qwerty123@cluster0.hciqlrl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-const connect= async () =>{
-    try{
-        await mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+const uri = 'mongodb+srv://shashikaladilini11:qwerty123@cluster0.hciqlrl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+const connect = async () => {
+    try {
+        await mongoose.connect(uri);
         console.log('Connected to database');
-      
-    }catch(err){
-        console.log(err);
+    } catch (err) {
+        console.error('Connection to database failed:', err);
+        process.exit(1); // Exit process on connection failure
     }
-}
+};
+
 connect();
 
-
-const server=app.listen(port, host, () => {
-    console.log(`Server is running on ${server.address().port}`);
+const server = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
 
-
-
-app.use('/api',router);
+app.use('/api', router);
