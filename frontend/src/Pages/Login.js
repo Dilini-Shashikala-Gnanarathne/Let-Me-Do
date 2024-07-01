d5093d317c8b6a1d14d34b9c12c47bc6262473cf
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-07-01 18:57:31 +0530

    Signup Frontend

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -1,55 +1,28 @@
-import React from 'react';
 import { useState } from 'react';
 import '../App.css'
 const Login = () => {
     const [formData,setFormData] =useState({
-            id: '',
-            name: '',
-            password: '',
-            email: '',	});
-    
-            const handleInputChange = (e) => {
-                const { name, value } = e.target;
-                setFormData((prevData) => ({
-                  ...prevData,
-                  [name]: value,
-                }));
-              };
-            
-              const handleSubmit = (e) => {
-                e.preventDefault();
-                alert("hi");
-              };
-            
+        id: '',
+        name: '',
+        password: '',
+        email: '',	});
+
+        const handleInputChange = (e) => {
+            const { name, value } = e.target;
+            setFormData((prevData) => ({
+              ...prevData,
+              [name]: value,
+            }));
+          };
+        
+          const handleSubmit = (e) => {
+            e.preventDefault();
+            alert("hi");
+          };
   return (
     <div className="container">
-        <h1>
-            Create an <span>Account</span>
-        </h1>
-        <form onSubmit={handleSubmit}>
-              <div className="form-group">
-                <label htmlFor="id">Enter Your Name</label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  value={formData.name}
-                  onChange={handleInputChange}
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="name">Enter Your Register Number</label>
-                <input
-                  type="text"
-                  id="id"
-                  name="id"
-                value={formData.id}
-                onChange={handleInputChange}
-                  required
-                />
-              </div>
-              <div className="form-group">
+      <form>
+      <div className="form-group">
               <label htmlFor="id">Enter Your Email</label>
                 <input
                   type="text"
@@ -72,7 +45,7 @@ const Login = () => {
                 />
               </div>
               <button type="submit">Add</button>
-            </form>
+      </form>
     </div>
   )
 }
