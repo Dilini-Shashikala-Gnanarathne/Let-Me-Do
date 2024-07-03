623f6f02ca24fdb15ca69cf0a0c9241719666f3d
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-07-03 23:06:13 +0530

    bycription

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -1,53 +1,58 @@
-const User =require('../models/UserSchema');
-const Admin = require('../models/AdminSchema');
+const User = require('../models/UserSchema'); // Adjust the path as per your project structure
+const Admin = require('../models/AdminSchema'); // Adjust the path as per your project structure
+const bcrypt = require('bcrypt');
 
+const register = async (req, res) => {
+    const { email, password, name, role } = req.body;
 
-
- const register = async(req,res)=>{
-
-    const {email,password,name,role} = req.body
-
-    try{
-
-        let user=null
-
-        if(role== 'viewer'){
-            user=await User.findOne({email})
-        }
-        else if(role=='admin'){
-            user = await Admin.findOne({email})
+    try {
+        // Check if user already exists based on role
+        let user = null;
+        if (role === 'viewer') {
+            user = await User.findOne({ email });
+        } else if (role === 'admin') {
+            user = await Admin.findOne({ email });
         }
 
-        //check if user exist
-        if(user){
-            return res.status(400).json({message:'User already exist'})
+        // If user already exists, return error response
+        if (user) {
+            return res.status(400).json({ success: false, message: 'User already exists' });
         }
-       
-        if(role=='viewer'){
+
+        // Hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        // Create new user instance based on role
+        if (role === 'viewer') {
             user = new User({
                 name,
                 email,
-                password,
+                password: hashPassword,
                 role
-            })
-        }
-
-        if(role=='admin'){
+            });
+        } else if (role === 'admin') {
             user = new Admin({
                 name,
                 email,
-                password,
+                password: hashPassword,
                 role
-            })
+            });
         }
 
+        // Save user to database
         await user.save();
 
-        res.status(200).json({success:true,message: 'User successfully created'});
+        // Return success response
+        res.status(200).json({ success: true, message: 'User successfully created' });
 
-    }catch(err){
-        res.status(500).json({success:false,message: err});
+    } catch (err) {
+        // Handle errors
+        console.error(err.message);
+        res.status(500).json({ success: false, message: 'Internal server error, Please try again' });
     }
 };
 
-exports.register = register;
+module.exports = {
+    register
+};
