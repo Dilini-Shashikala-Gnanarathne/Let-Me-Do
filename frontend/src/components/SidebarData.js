0ca6ce651d89a1a325e13bff0b7047782bf21851
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-07-04 22:43:16 +0530

    add result page

--- frontend/src/components/SidebarData.js
+++ frontend/src/components/SidebarData.js
@@ -1,19 +1,25 @@
 import React from "react";
 
 import DashboardIcon from "../assets/home.png";
-import ReportsIcon from "../assets/result.png";
-
+import ResultIcon from "../assets/result.png";
+import ReportsIcon from "../assets/report.png";
 export const SidebarData = [
   {
     title: "Home",
-    path: "/",
+    path: "/home",
     icon: <img src={DashboardIcon} alt="Dashboard" className="dashboard-icon"/>,
     cName: "nav-text dashboard",
   },
   {
     title: "Result",
     path: "/Result",
-    icon: <img src={ReportsIcon} alt="Result" className="dashboard-icon"/>,
+    icon: <img src={ResultIcon} alt="Result" className="dashboard-icon"/>,
+    cName: "nav-text dashboard",
+  },
+  {
+    title: "Report",
+    path: "/Report",
+    icon: <img src={ReportsIcon} alt="Dashboard" className="dashboard-icon"/>,
     cName: "nav-text dashboard",
   },
   
