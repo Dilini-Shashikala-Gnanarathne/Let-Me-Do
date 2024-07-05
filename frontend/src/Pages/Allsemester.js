d53ec40955c42370077fb64f1720bec69e302726
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-07-05 22:45:36 +0530

    add all items

--- frontend/src/Pages/Allsemester.js
+++ frontend/src/Pages/Allsemester.js
@@ -32,6 +32,91 @@ const Allsemester = () => {
       </div>
     </div>
 
+    <h3 className='first'>Second Year</h3>
+    <div className="container-all-sem-wrapper">
+      <div className="container-all-sem">
+        <p className="title-all">Calculate 1st Semester GPA</p>
+        <div className="form-group">
+          <label htmlFor="numCourses">Number of Courses</label>
+          <input
+            type="number"
+            id="numCourses"
+            name="numCourses"
+            required
+          />
+        </div>
+      </div>
+      <div className="container-all-sem">
+        <p className="title-all">Calculate 2nd Semester GPA</p>
+        <div className="form-group">
+          <label htmlFor="numCourses">Number of Courses</label>
+          <input
+            type="number"
+            id="numCourses"
+            name="numCourses"
+            required
+          />
+        </div>
+      </div>
+    </div>
+
+    <h3 className='first'>Third Year</h3>
+    <div className="container-all-sem-wrapper">
+      <div className="container-all-sem">
+        <p className="title-all">Calculate 1st Semester GPA</p>
+        <div className="form-group">
+          <label htmlFor="numCourses">Number of Courses</label>
+          <input
+            type="number"
+            id="numCourses"
+            name="numCourses"
+            required
+          />
+        </div>
+      </div>
+      <div className="container-all-sem">
+        <p className="title-all">Calculate 2nd Semester GPA</p>
+        <div className="form-group">
+          <label htmlFor="numCourses">Number of Courses</label>
+          <input
+            type="number"
+            id="numCourses"
+            name="numCourses"
+            required
+          />
+        </div>
+      </div>
+    </div>
+
+    <h3 className='first'>Fourth Year</h3>
+    <div className="container-all-sem-wrapper">
+      <div className="container-all-sem">
+        <p className="title-all">Calculate 1st Semester GPA</p>
+        <div className="form-group">
+          <label htmlFor="numCourses">Number of Courses</label>
+          <input
+            type="number"
+            id="numCourses"
+            name="numCourses"
+            required
+          />
+        </div>
+      </div>
+      <div className="container-all-sem">
+        <p className="title-all">Calculate 2nd Semester GPA</p>
+        <div className="form-group">
+          <label htmlFor="numCourses">Number of Courses</label>
+          <input
+            type="number"
+            id="numCourses"
+            name="numCourses"
+            required
+          />
+        </div>
+      </div>
+    </div>
+
+
 
     
     </>
