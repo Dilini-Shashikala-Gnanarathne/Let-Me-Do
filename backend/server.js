43ecc975adadbccdb005f21b8e4acac1b69a3f9c
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-07-06 17:15:55 +0530

    Server Security

--- backend/server.js
+++ backend/server.js
@@ -4,26 +4,29 @@ const cors = require('cors');
 const port = 3001;
 const mongoose = require('mongoose');
 const router = require('./router');
+const dotenv =require('dotenv');
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
 
-const uri = 'mongodb+srv://shashikaladilini11:qwerty123@cluster0.hciqlrl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-
-const connect = async () => {
+const connectDB = async () => {
     try {
-        await mongoose.connect(uri);
-        console.log('Connected to database');
+        await mongoose.connect(process.env.Mongo_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB database is connected');
     } catch (err) {
-        console.error('Connection to database failed:', err);
-        process.exit(1); // Exit process on connection failure
+        console.error('MongoDB database connection failed:', err); 
     }
 };
 
-connect();
 
-const server = app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+
+app.listen(port, () => {
+    connectDB();
+    console.log("Server is running on port " + port);
 });
 
 app.use('/api', router);
