1690310bbf566078644c4446c115034cf3c8abdb
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-07-07 19:12:46 +0530

    Fix errors

--- frontend/src/Pages/All Semester Pages/SecondYearSecond.js
+++ frontend/src/Pages/All Semester Pages/SecondYearSecond.js
@@ -1,98 +1,172 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import '../../App.css';
+import Background from '../../components/D-Background';
+const Dashboard = ({ data, updateUser }) => {
+  const [courseData, setCourseData] = useState({
+    id: '',
+    name: '',
+    credit: '',
+  });
+  const [numCourses, setNumCourses] = useState('');
+  const [submissionCount, setSubmissionCount] = useState(0);
+  const [isEditing, setIsEditing] = useState(false);
+  const [formVisible, setFormVisible] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  const handleAddClick = () => {
+    if (submissionCount < 9) {
+        setSubmissionCount(submissionCount + 1);
+    }
+  };
+  useEffect(() => {
+    if (data?.id && data.id !== 0) {
+      setCourseData({
+        id: data.id,
+        name: data.name,
+        credit: data.credit,
+      });
+      setIsEditing(true);
+    }
+  }, [data]);
 
-const SecondYearSecond = () => {
-    return (
-        <>
-    <Background/>
-      <div >
-        {!formVisible ? (
-          <div className="container">
-            <h3 className="title">Calculate Semester GPA</h3>
-            <div className="form-group">
-              <label htmlFor="numCourses">Number of Courses</label>
-              <input
-                type="number"
-                id="numCourses"
-                name="numCourses"
-                value={numCourses}
-                onChange={(e) => setNumCourses(e.target.value)}
-                required
-              />
-            </div>
-            <button className='login-button'onClick={handleStart}>Enter</button>
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setCourseData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isEditing) {
+      updateUser(courseData);
+    } else {
+      addUser(courseData);
+    }
+  };
+
+  const addUser = (data) => {
+    Axios.post('http://localhost:3001/api/create', data)
+      .then(() => {
+        setSubmissionCount((prevCount) => prevCount + 1);
+        resetForm();
+        setError(null);
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 400) {
+          setError(error.response.data.error);
+        } else {
+          setError('An unexpected error occurred');
+        }
+      });
+  };
+
+  const resetForm = () => {
+    setCourseData({
+      id: '',
+      name: '',
+      credit: '',
+    });
+  };
+
+  const handleStart = () => {
+    setFormVisible(true);
+  };
+
+  return (
+    <>
+<Background/>
+  <div >
+    {!formVisible ? (
+      <div className="container">
+        <h3 className="title">Calculate Semester GPA</h3>
+        <div className="form-group">
+          <label htmlFor="numCourses">Number of Courses</label>
+          <input
+            type="number"
+            id="numCourses"
+            name="numCourses"
+            value={numCourses}
+            onChange={(e) => setNumCourses(e.target.value)}
+            required
+          />
+        </div>
+        <button className='login-button'onClick={handleStart}>Enter</button>
+      </div>
+    ) : (
+      <div>
+        
+        {submissionCount < numCourses ? (
+        <div className="container-Add">
+        <form>
+          <h3 className="title">Add Course Details</h3>
+          <div className="form-group">
+            <label htmlFor="id" className='label-title'>Course Code: IS100{submissionCount + 1}</label>
           </div>
-        ) : (
-          <div>
-            
-            {submissionCount < numCourses ? (
-            <div className="container-Add">
-            <form>
-              <h3 className="title">Add Course Details</h3>
-              <div className="form-group">
-                <label htmlFor="id" className='label-title'>Course Code: IS100{submissionCount + 1}</label>
-              </div>
-              <div className="form-group">
-                <label htmlFor="name">Enter Subject Grade</label>
-                <select
-                  id="name"
-                  name="name"
-                  required
-                  className="form-control"
-                >
-                  <option value="">Select Grade</option>
-                  <option value="A+">A+</option>
-                  <option value="A">A</option>
-                  <option value="A-">A-</option>
-                  <option value="B+">B+</option>
-                  <option value="B">B</option>
-                  <option value="B-">B-</option>
-                  <option value="C+">C+</option>
-                  <option value="C">C</option>
-                  <option value="C-">C-</option>
-                  <option value="D+">D+</option>
-                  <option value="D">D</option>
-                  <option value="D-">D-</option>
-                  <option value="E">E</option>
-                </select>
-              </div>
-              <div className="form-group">
-                <label htmlFor="credit">Enter Number of Credits</label>
-                <select
-                  id="credit"
-                  name="credit"
-                  required
-                  className="form-control"
-                >
-                  <option value="">Select Credits</option>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                </select>
-              </div>
-              <div className="form-group">
-                <button type="button" onClick={handleAddClick}>
-                  Add
-                </button>
-              </div>
-            </form>
+          <div className="form-group">
+            <label htmlFor="name">Enter Subject Grade</label>
+            <select
+              id="name"
+              name="name"
+              required
+              className="form-control"
+            >
+              <option value="">Select Grade</option>
+              <option value="A+">A+</option>
+              <option value="A">A</option>
+              <option value="A-">A-</option>
+              <option value="B+">B+</option>
+              <option value="B">B</option>
+              <option value="B-">B-</option>
+              <option value="C+">C+</option>
+              <option value="C">C</option>
+              <option value="C-">C-</option>
+              <option value="D+">D+</option>
+              <option value="D">D</option>
+              <option value="D-">D-</option>
+              <option value="E">E</option>
+            </select>
           </div>
-        ) : (
-          <div className="container-end">
-                <h3 className="title"> 
-                  <div className="form-end">
-                    <p className='check-text'>You successfully added {submissionCount} courses </p>
-                    <p className='checkmark'>✨</p>         
-                  </div>
-                </h3>
-            </div>
-            )}
+          <div className="form-group">
+            <label htmlFor="credit">Enter Number of Credits</label>
+            <select
+              id="credit"
+              name="credit"
+              required
+              className="form-control"
+            >
+              <option value="">Select Credits</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+            </select>
           </div>
+          <div className="form-group">
+            <button type="button" onClick={handleAddClick}>
+              Add
+            </button>
+          </div>
+        </form>
+      </div>
+    ) : (
+      <div className="container-end">
+            <h3 className="title"> 
+              <div className="form-end">
+                <p className='check-text'>You successfully added {submissionCount} courses </p>
+                <p className='checkmark'>✨</p>         
+              </div>
+            </h3>
+        </div>
         )}
       </div>
-        
-        </>
-      );
-    };
+    )}
+  </div>
     
+    </>
+  );
+};
 
-export default SecondYearSecond
+export default Dashboard;
--- frontend/src/Pages/Allsemester.js
+++ frontend/src/Pages/Allsemester.js
@@ -1,60 +1,71 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom'; 
 import './Allsemester.css';
-import AddResult2 from './AddResult2';
 
-const SemesterGPAForm = ({ semesterName, handleAddResult }) => {
+const SemesterGPAForm = ({ semesterName, pageName }) => {
+  const getLinkPath = () => {
+    switch (pageName) {
+      case '1':
+        return '/firstfirst';
+      case '2':
+        return '/firstsecond';
+      case '3':
+        return '/secondfirst';
+      case '4':
+        return '/secondsecond';
+      case '5':
+        return '/addresult5';
+      case '6':
+        return '/addresult6';
+      case '7':
+        return '/addresult7';
+      case '8':
+        return '/addresult8';
+      default:
+        return '/';
+    }
+  };
+
   return (
     <div className="container-all-sem">
-      <>
-        <p className="title-all">Calculate {semesterName} Semester GPA</p>
-        <form className="form-group">
-          <label htmlFor="numCourses">Add Your Results</label>
-          <button type="button" className='add-result' onClick={handleAddResult}>Add</button>
-        </form>
-      </>
+      <p className="title-all">Calculate {semesterName} Semester GPA</p>
+      <form className="form-group">
+        <label htmlFor="numCourses">Add Your Results</label>
+        <Link to={getLinkPath()}>
+          <button type="button" className='add-result'>Add</button>
+        </Link>
+      </form>
     </div>
   );
 };
 
 const Allsemester = () => {
-  const [showAddResult, setShowAddResult] = useState(false);
-
-  const handleAddResult = () => {
-    setShowAddResult(true);
-    console.log('Add Result clicked');
-  };
-
-  if (showAddResult) {
-    return <AddResult2 />;
-  }
-
   return (
     <>
       <h3 className='first'>First Year</h3>
       <div className="container-all-sem-wrapper">
-        <SemesterGPAForm semesterName="1st" handleAddResult={handleAddResult} />
-        <SemesterGPAForm semesterName="2nd" handleAddResult={handleAddResult} />
+        <SemesterGPAForm semesterName="1st" pageName="1" />
+        <SemesterGPAForm semesterName="2nd" pageName="2" />
       </div>
 
       <h3 className='first'>Second Year</h3>
       <div className="container-all-sem-wrapper">
-        <SemesterGPAForm semesterName="1st" handleAddResult={handleAddResult} />
-        <SemesterGPAForm semesterName="2nd" handleAddResult={handleAddResult} />
+        <SemesterGPAForm semesterName="1st" pageName="3" />
+        <SemesterGPAForm semesterName="2nd" pageName="4" />
       </div>
 
       <h3 className='first'>Third Year</h3>
       <div className="container-all-sem-wrapper">
-        <SemesterGPAForm semesterName="1st" handleAddResult={handleAddResult} />
-        <SemesterGPAForm semesterName="2nd" handleAddResult={handleAddResult} />
+        <SemesterGPAForm semesterName="1st" pageName="5" />
+        <SemesterGPAForm semesterName="2nd" pageName="6" />
       </div>
 
       <h3 className='first'>Fourth Year</h3>
       <div className="container-all-sem-wrapper">
-        <SemesterGPAForm semesterName="1st" handleAddResult={handleAddResult} />
-        <SemesterGPAForm semesterName="2nd" handleAddResult={handleAddResult} />
+        <SemesterGPAForm semesterName="1st" pageName="7" />
+        <SemesterGPAForm semesterName="2nd" pageName="8" />
       </div>
     </>
   );
 };
-
 export default Allsemester;
