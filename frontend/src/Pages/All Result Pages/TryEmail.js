5ed196a603f4e79d5178fa37ec6d0995a4334972
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-07-15 14:57:01 +0530

    change styles of result page

--- frontend/src/Pages/All Result Pages/TryEmail.js
+++ frontend/src/Pages/All Result Pages/TryEmail.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useAuth, useEmail } from '../../context/AuthContext';
-
+import './AllResult.css';
 const UserProfile = () => {
   const { user } = useAuth();
   const email = useEmail();
@@ -11,24 +11,25 @@ const UserProfile = () => {
 
   const renderSemesters = (semesterData) => {
     return semesterData.map((semester, index) => (
-      <div key={index}>
-        <h3>Semester {index + 1}</h3>
-        {semester.map((subject, subIndex) => (
+      <div className='container-all-sem-result'>
+      <div key={index} >
+        <h3 className='semester' >Semester {index + 1}</h3>
+       <div className='form-group-result'> {semester.map((subject, subIndex) => (
           <div key={subject._id || subIndex}>
-            <p>Subject: {subject.subject}</p>
+            <p className='semester-p'>Subject: {subject.subject}</p>
             <p>Grade: {subject.grade}</p>
           </div>
-        ))}
-      </div>
+        ))}</div>
+      </div></div>
     ));
   };
 
   return (
-    <div className="container-Add">
-      <h1>Welcome, {user.name}</h1>
-      <p>Email: {email}</p>
+    <div className="container-Add-result">
+      <h1 className='title-all-result'>Welcome, {user.name}</h1>
+      <p>Register Number: {user.id}</p>
       <div>
-        <h2>Semesters:</h2>
+        <h2>Your Results:</h2>
         {renderSemesters([
           user.firstyearfirst,
           user.firstyearsecond,
