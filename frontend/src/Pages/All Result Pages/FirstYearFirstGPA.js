0cf086e97ff3d042c215f372ed4db334cb4848c5
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-07-16 17:32:02 +0530

    Fixed errors of buttons

--- frontend/src/Pages/All Result Pages/FirstYearFirstGPA.js
+++ frontend/src/Pages/All Result Pages/FirstYearFirstGPA.js
@@ -1,167 +1,166 @@
 import React from 'react';
-import { useAuth, useEmail } from '../../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 import './AllResult.css';
-const FirstYearFirstGPA = () => {
+
+// First Year First Semester GPA
+const FirstYearFirstSemesterGPA = () => {
   const { user } = useAuth();
 
   if (!user) {
     return <div>Loading...</div>;
-  };
+  }
 
   return (
     <div className="container-Add-result">
       <h1 className='title-all-result'>Welcome, {user.name}</h1>
       <p>Register Number: {user.id}</p>
       <div>
-       <p>FirstYearFirstGPA:{user.firstyearfirstGPA}</p>
+        <p>First Year First Semester GPA: {user.firstyearfirstGPA}</p>
       </div>
     </div>
   );
 };
 
-FirstYearFirstSemesterGPA= () => {
+// First Year Second Semester GPA
+const FirstYearSecondSemesterGPA = () => {
   const { user } = useAuth();
 
   if (!user) {
     return <div>Loading...</div>;
-  };
+  }
 
   return (
     <div className="container-Add-result">
       <h1 className='title-all-result'>Welcome, {user.name}</h1>
       <p>Register Number: {user.id}</p>
       <div>
-       <p>FirstYearFirstGPA:{user.firstyearfirstGPA}</p>
+        <p>First Year Second Semester GPA: {user.firstyearsecondGPA}</p>
       </div>
     </div>
   );
 };
- 
-FirstYearSecondSemesterGPA= () => {
+
+// Second Year First Semester GPA
+const SecondYearFirstSemesterGPA = () => {
   const { user } = useAuth();
 
   if (!user) {
     return <div>Loading...</div>;
-  };
+  }
 
   return (
     <div className="container-Add-result">
       <h1 className='title-all-result'>Welcome, {user.name}</h1>
       <p>Register Number: {user.id}</p>
       <div>
-       <p>FirstYearFirstGPA:{user.firstyearfirstGPA}</p>
+        <p>Second Year First Semester GPA: {user.secondyearfirstGPA}</p>
       </div>
     </div>
   );
 };
 
-SecondYearFirstSemesterGPA= () => {
+// Second Year Second Semester GPA
+const SecondYearSecondSemesterGPA = () => {
   const { user } = useAuth();
 
   if (!user) {
     return <div>Loading...</div>;
-  };
+  }
 
   return (
     <div className="container-Add-result">
       <h1 className='title-all-result'>Welcome, {user.name}</h1>
       <p>Register Number: {user.id}</p>
       <div>
-       <p>FirstYearFirstGPA:{user.firstyearfirstGPA}</p>
+        <p>Second Year Second Semester GPA: {user.secondyearsecondGPA}</p>
       </div>
     </div>
   );
 };
 
-SecondYearSecondSemesterGPA= () => {
+// Third Year First Semester GPA
+const ThirdYearFirstSemesterGPA = () => {
   const { user } = useAuth();
 
   if (!user) {
     return <div>Loading...</div>;
-  };
+  }
 
   return (
     <div className="container-Add-result">
       <h1 className='title-all-result'>Welcome, {user.name}</h1>
       <p>Register Number: {user.id}</p>
       <div>
-       <p>FirstYearFirstGPA:{user.firstyearfirstGPA}</p>
+        <p>Third Year First Semester GPA: {user.thirdyearfirstGPA}</p>
       </div>
     </div>
   );
 };
 
-ThirdYearFirstSemesterGPA= () => {
+// Third Year Second Semester GPA
+const ThirdYearSecondSemesterGPA = () => {
   const { user } = useAuth();
 
   if (!user) {
     return <div>Loading...</div>;
-  };
+  }
 
   return (
     <div className="container-Add-result">
       <h1 className='title-all-result'>Welcome, {user.name}</h1>
       <p>Register Number: {user.id}</p>
       <div>
-       <p>FirstYearFirstGPA:{user.firstyearfirstGPA}</p>
+        <p>Third Year Second Semester GPA: {user.thirdyearsecondGPA}</p>
       </div>
     </div>
   );
 };
 
-ThirdYearSecondSemesterGPA= () => {
+// Fourth Year First Semester GPA
+const FourthYearFirstSemesterGPA = () => {
   const { user } = useAuth();
 
   if (!user) {
     return <div>Loading...</div>;
-  };
+  }
 
   return (
     <div className="container-Add-result">
       <h1 className='title-all-result'>Welcome, {user.name}</h1>
       <p>Register Number: {user.id}</p>
       <div>
-       <p>FirstYearFirstGPA:{user.firstyearfirstGPA}</p>
+        <p>Fourth Year First Semester GPA: {user.fourthyearfirstGPA}</p>
       </div>
     </div>
   );
 };
 
-FourthYearFirstSemesterGPA= () => {
+// Fourth Year Second Semester GPA
+const FourthYearSecondSemesterGPA = () => {
   const { user } = useAuth();
 
   if (!user) {
     return <div>Loading...</div>;
-  };
+  }
 
   return (
     <div className="container-Add-result">
       <h1 className='title-all-result'>Welcome, {user.name}</h1>
       <p>Register Number: {user.id}</p>
       <div>
-       <p>FirstYearFirstGPA:{user.firstyearfirstGPA}</p>
+        <p>Fourth Year Second Semester GPA: {user.fourthyearsecondGPA}</p>
       </div>
     </div>
   );
 };
 
-FourthYearSecondSemesterGPA= () => {
-  const { user } = useAuth();
-
-  if (!user) {
-    return <div>Loading...</div>;
-  };
-
-  return (
-    <div className="container-Add-result">
-      <h1 className='title-all-result'>Welcome, {user.name}</h1>
-      <p>Register Number: {user.id}</p>
-      <div>
-       <p>FirstYearFirstGPA:{user.firstyearfirstGPA}</p>
-      </div>
-    </div>
-  );
+export {
+  FirstYearFirstSemesterGPA,
+  FirstYearSecondSemesterGPA,
+  SecondYearFirstSemesterGPA,
+  SecondYearSecondSemesterGPA,
+  ThirdYearFirstSemesterGPA,
+  ThirdYearSecondSemesterGPA,
+  FourthYearFirstSemesterGPA,
+  FourthYearSecondSemesterGPA
 };
-
-
-export default FirstYearFirstGPA;
