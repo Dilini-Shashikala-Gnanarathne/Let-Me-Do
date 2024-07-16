1a0607223c0ec4e6aa60913cb39bd66b87becf79
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-07-16 09:34:26 +0530

    GPA condition added

--- backend/models/UserSchema.js
+++ backend/models/UserSchema.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const semesterSchema = new Schema({
-  subject: { type: String ,unique: true},
+  subject: { type: String },
   grade: { type: String },
-  subjectcredit:{type: Number, min: 0, max:7}
+  subjectcredit: { type: Number, min: 0, max: 7 }
 });
 
 const userSchema = new Schema({
@@ -22,23 +22,14 @@ const userSchema = new Schema({
   fourthyearfirst: [semesterSchema],
   fourthyearsecond: [semesterSchema],
   firstyearfirstGPA: { type: Number },
-
   firstyearsecondGPA: { type: Number },
-
   secondyearfirstGPA: { type: Number },
-
   secondyearsecondGPA: { type: Number },
-
   thirdyearfirstGPA: { type: Number },
-
   thirdyearsecondGPA: { type: Number },
-
   fourthyearfirstGPA: { type: Number },
-
   fourthyearsecondGPA: { type: Number },
 });
-
-
 
 const User = mongoose.model('User', userSchema);
 
--- frontend/src/components/SidebarData.js
+++ frontend/src/components/SidebarData.js
@@ -67,7 +67,7 @@ export const SidebarData = [
   },
   {
     title: "Report",
-    path: "/report",
+    path: 'getfirstfirst',
     icon: <img src={ReportsIcon} alt="Dashboard" className="dashboard-icon"/>,
     cName: "nav-text dashboard",
   },
