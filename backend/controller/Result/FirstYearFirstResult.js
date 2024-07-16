33866bd4ff622e1e211ab5554c9d318bd5fa3d5c
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-07-16 16:37:56 +0530

    changed API

--- backend/controller/Result/FirstYearFirstResult.js
+++ backend/controller/Result/FirstYearFirstResult.js
@@ -81,8 +81,8 @@ const getSemester = async (req, res) => {
   }
 };
 
-const getfirstyearfirst = (req, res) => getSemester(req, res);
+const getFirstYearFirstSemesterGPA = (req, res) => getSemester(req, res);
 
 module.exports = {
-  getfirstyearfirst,
+  getFirstYearFirstSemesterGPA,
 };
