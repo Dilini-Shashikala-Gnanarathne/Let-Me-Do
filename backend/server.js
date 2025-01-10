8d8bbd8491ee26de0845562502311504051a6deb
sudeepa99 <sudeepa1234nisal@gmail.com>
2025-01-10 21:20:23 +0530

    build done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const router = require('./router');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const router = require("./router");
 
 dotenv.config();
 
@@ -13,29 +13,34 @@ app.use(cors());
 app.use(express.json());
 
 // Check if the environment variable is loaded correctly
-console.log('MongoDB URI:', process.env.Mongo_URL);
+console.log("MongoDB URI:", process.env.Mongo_URL);
 console.log(1);
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.Mongo_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB database is connected');
-    } catch (err) {
-        console.error('MongoDB database connection failed:', err);
-        process.exit(1); 
-    }
+  try {
+    await mongoose.connect(process.env.Mongo_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB database is connected");
+  } catch (err) {
+    console.error("MongoDB database connection failed:", err);
+    process.exit(1);
+  }
 };
 
 const startServer = async () => {
-    await connectDB(); 
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-    });
+  await connectDB();
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 };
 
-app.use('/api', router);
+app.use("/api", router);
+
+app.use(express.static("./frontend/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
 
 startServer();
