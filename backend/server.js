85c40aa187bb5edccd50470a5ca32ba172771ea0
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2025-01-11 21:37:29 +0530

    Update server response to serve server.html instead of index.html

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ app.use("/api", router);
 
 app.use(express.static("./backend/build"));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "backend", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "backend", "build", "server.html"));
 });
 
 startServer();
